7a420346eb6ea7e3a85b3ae94ac4b1926a62352e
ruoN23 <aboubakarinourislam@gmail.com>
2026-03-14 19:57:41 +0000

    modification du ci pour les pulls requests

--- backend/src/SmartCV.API/obj/Debug/net10.0/SmartCV.API.AssemblyInfo.cs
+++ backend/src/SmartCV.API/obj/Debug/net10.0/SmartCV.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartCV.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbf86ff0fcfb8fcebcbf3106d967f48e618d20fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b118090c9217d9e064bf9bd99be15e88af8446e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartCV.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartCV.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
