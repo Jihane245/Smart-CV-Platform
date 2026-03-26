acd17ca26307f387b540c97ac2ee39b180c125f9
Atae <ahmadoun.atae@etu.uae.ac.ma>
2026-03-26 22:06:48 +0100

    Fix pull

--- backend/src/SmartCV.API/obj/Debug/net10.0/SmartCV.API.AssemblyInfo.cs
+++ backend/src/SmartCV.API/obj/Debug/net10.0/SmartCV.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartCV.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbf86ff0fcfb8fcebcbf3106d967f48e618d20fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+380b996a528e3374dbb44d13b54195feb28c962f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartCV.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartCV.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
