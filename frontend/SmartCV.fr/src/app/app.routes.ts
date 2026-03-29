d3aa08f859abf5c31c174fa2cac66ecf6fe8ac49
Atae <ahmadoun.atae@etu.uae.ac.ma>
2026-03-29 04:11:29 +0100

    Removed Keycloak (for now), add chassis for user pages, restructured frontend auth

--- frontend/SmartCV.fr/src/app/app.routes.ts
+++ frontend/SmartCV.fr/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
-import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'auth', pathMatch: 'full' },
+  {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full',
+  },
   {
     path: 'auth',
     children: [
@@ -15,12 +18,64 @@ export const routes: Routes = [
       },
       {
         path: 'connexion',
-         loadComponent: () =>
-           import('./features/auth/pages/connexion/connexion').then(
-             (m) => m.Connexion
-           ),
-       },
-      { path: '', redirectTo: 'inscription', pathMatch: 'full' },
+        loadComponent: () =>
+          import('./features/auth/pages/connexion/connexion').then(
+            (m) => m.Connexion
+          ),
+      },
+      {
+        path: '',
+        redirectTo: 'inscription',
+        pathMatch: 'full',
+      },
     ],
+  },
+  {
+    path: 'user',
+    loadComponent: () =>
+      import('./shared/layout/sidebar/sidebar.component').then(
+        (m) => m.SidebarComponent
+      ),
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./features/user/pages/home/home').then(
+            (m) => m.Home
+          ),
+      },
+      {
+        path: 'profile',
+        loadComponent: () =>
+          import('./features/user/pages/profile/profile').then(
+            (m) => m.Profile
+          ),
+      },
+      {
+        path: 'generate-cv',
+        loadComponent: () =>
+          import('./features/user/pages/generate-cv/generate-cv').then(
+            (m) => m.GenerateCv
+          ),
+      },
+      {
+        path: 'applications',
+        loadComponent: () =>
+          import('./features/user/pages/applications/applications').then(
+            (m) => m.Applications
+          ),
+      },
+      {
+        path: 'settings',
+        loadComponent: () =>
+          import('./features/user/pages/settings/settings').then(
+            (m) => m.Settings
+          ),
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'auth',
   },
 ];
